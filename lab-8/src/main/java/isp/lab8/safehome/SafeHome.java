ee7bdec442515e72330d7faef77bece3671dc63e
Marius Pașcalău <marius.pascalau@codemart.ro>
2020-05-05 23:54:10 +0300

    solve lab8

--- lab-8/src/main/java/isp/lab8/safehome/SafeHome.java
+++ lab-8/src/main/java/isp/lab8/safehome/SafeHome.java
@@ -3,8 +3,6 @@ package isp.lab8.safehome;
 public class SafeHome {
 
     public static void main(String[] args) {
-        DeserializationService deserializationService = new DeserializationService();
-        deserializationService.deserializeFiles();
-        deserializationService.getAccessLogs().forEach(System.out::println);
+        DeserializationService.deserializeFiles().forEach(System.out::println);
     }
 }
